960b45b3a9125383ec6101da2c43a589ace50901
T1@n <140062774+t1antlan@users.noreply.github.com>
2024-09-19 23:31:13 +0800

    Add files via upload

---
+++ Web-F12/src/index.php
@@ -0,0 +1,25 @@
+<!--
+# -*- coding: utf-8 -*-
+# @Author: h1xa
+# @Date:   2020-09-01 13:45:32
+# @Last Modified by:   h1xa
+# @Last Modified time: 2020-09-02 03:12:48
+# @email: h1xa@ctfer.com
+# @link: https://ctfer.com
+
+-->
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>CHNU招新 </title>
+<script type="text/javascript">
+
+</script>
+</head>
+<body>
+	<h3>where is flag?</h3>
+	<!-- flag{8a80572c-d336-4702-ac64-478a7b267c25} -->
+</body>
+</html>
